523ce98d92e003997320e15fe858d5459fe53dc6
pikachoo1305 <134606221+pikachoo1305@users.noreply.github.com>
2023-06-23 14:55:26 +0400

    первая правка

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="left-panel">
+      <h2>Добавление нового навыка</h2>
+      <form @submit.prevent="addSkill">
+        <div class="form-group">
+          <label for="skillName">Название навыка</label>
+          <input type="text" id="skillName" v-model="newSkill.name" required>
+        </div>
+        <div class="form-group">
+          <label for="skillLevel">Уровень владения</label>
+          <select id="skillLevel" v-model="newSkill.level" required>
+            <option value="Начинающий">Начинающий</option>
+            <option value="Средний">Средний</option>
+            <option value="Продвинутый">Продвинутый</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="skillDescription">Описание навыка</label>
+          <textarea id="skillDescription" v-model="newSkill.description"></textarea>
+        </div>
+        <button type="submit">Добавить навык</button>
+      </form>
+    </div>
+    <div class="right-panel">
+      <h2>Список навыков</h2>
+      <ul>
+        <li v-for="skill in skills" :key="skill.id">
+          <h3>{{ skill.name }}</h3>
+          <p>Уровень владения: {{ skill.level }}</p>
+          <p>{{ skill.description }}</p>
+          <button @click="removeSkill(skill)">Удалить</button>
+          <button @click="resetSkills">Обнулить все навыки</button><!-- Кнопка для обнуления всех навыков -->
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newSkill: {
+        name: '',
+        level: 'Начинающий',
+        description: ''
+      },
+      skills: []
+    };
+  },
+  methods: {
+    addSkill() {
+      this.skills.push(this.newSkill);
+      this.saveSkillsToStorage(); // Сохраняем навыки в localStorage
+      this.newSkill = {
+        name: '',
+        level: 'Начинающий',
+        description: ''
+      };
+    },
+    removeSkill(skill) {
+      const index = this.skills.indexOf(skill);
+      if (index > -1) {
+        this.skills.splice(index, 1);
+        this.saveSkillsToStorage(); // Сохраняем навыки в localStorage
+      }
+    },
+    saveSkillsToStorage() {
+      localStorage.setItem('skills', JSON.stringify(this.skills));
+    },
+    loadSkillsFromStorage() {
+      const storedSkills = localStorage.getItem('skills');
+      if (storedSkills) {
+        this.skills = JSON.parse(storedSkills);
+      }
+    }
+  },
+  mounted() {
+    this.loadSkillsFromStorage(); // Восстанавливаем навыки из localStorage при загрузке страницы
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style scoped>
+/* Стили компонента */
 </style>
